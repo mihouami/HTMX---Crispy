b5628fb084d38adcbe027254bffd83cf973b3298
Minou <am.mihoubi@gmail.com>
2024-03-05 12:48:48 +0100

    03-add widgets and helper

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,17 @@
 from django import forms
+from datetime import datetime
+from django.urls import reverse_lazy
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 class UniversityForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_action = reverse_lazy('index')
+        self.helper.form_method = 'POST'
+        self.helper.add_input(Submit('submit', 'Submit'))
+
     SUBJECT_CHOICES = (
         (1, 'Web Dev'),
         (2, 'Programming'),
@@ -9,5 +20,9 @@ class UniversityForm(forms.Form):
 
     name = forms.CharField()
     age = forms.IntegerField()
-    subject = forms.ChoiceField(choices=SUBJECT_CHOICES)
-    dob = forms.DateField()
+    subject = forms.ChoiceField(
+        choices=SUBJECT_CHOICES,
+        widget=forms.RadioSelect())
+    dob = forms.DateField(widget=forms.DateInput(attrs={'type': 'date', 'max':datetime.now().date()}))
+
+    
